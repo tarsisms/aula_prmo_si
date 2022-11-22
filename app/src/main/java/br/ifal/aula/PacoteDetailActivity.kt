6646b4a42032fcbb4df8f354f084685323b2ac4d
tarsisms <tarsis.souza@ifal.edu.br>
2022-11-22 20:11:02 -0300

    Aula 06 - Intents Implicitas

--- app/src/main/java/br/ifal/aula/PacoteDetailActivity.kt
+++ app/src/main/java/br/ifal/aula/PacoteDetailActivity.kt
@@ -1,7 +1,10 @@
 package br.ifal.aula
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 
 class PacoteDetailActivity : AppCompatActivity() {
@@ -13,5 +16,17 @@ class PacoteDetailActivity : AppCompatActivity() {
 
         val textViewTitle = findViewById<TextView>(R.id.pacote_detail_title)
         textViewTitle.text = pacote.titulo;
+
+        val button = findViewById<Button>(R.id.button_accessar)
+        button.setOnClickListener {
+
+            val sendIntent = Intent(Intent.ACTION_SEND).apply {
+                data = Uri.parse("sms:8299854854")
+                putExtra("sms_body", "TEXT")
+                type = "text/plain"
+            }
+
+            startActivity(sendIntent)
+        }
     }
 }
